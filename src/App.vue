04ae4e3280acef85001352f515ee4d685a65e68c
vic33tor <vic33tor.vic@gmail.com>
2022-12-21 13:59:52 +0100

    Inicio html

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
+  <MiCabecera />
   <div>
-    <h1>Hola</h1>
+    <MiDia />
+  </div>
+  <div>
+    <MiSemana />
+  </div>
+  <div>
+    <MiMes />
   </div>
 </template>
 
-<script setup></script>
+<script>
+import MiDia from "./components/MiDia.vue";
+import MiSemana from "./components/MiSemana.vue";
+import MiCabecera from "./components/MiCabecera.vue";
+import MiMes from "./components/MiMes.vue";
+export default {
+  components: { MiDia, MiSemana, MiCabecera, MiMes },
+};
+</script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
